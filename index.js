5356b0d1d8a622d94606593aa9821b09ae216525
Newton Manyisa <manyisanewton26@gmail.com>
2024-12-12 12:22:36 +0300

    finish

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to attach the event listener
+  addingEventListener();
+  
